debe5b7ba1dff796ad053f44b64bf4361401600a
Hendrikto <hendrik.to@gmail.com>
2016-10-19 15:10:21 +0200

    Add a main class.

---
+++ src/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * @author Hendrik Werner // s4549775
+ * @author Constantin Blach // s4329872
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
