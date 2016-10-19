d2b824156cc4d23c4b61ee65f12b94b18b386b44
Hendrikto <hendrik.to@gmail.com>
2016-10-19 22:39:53 +0200

    Add tests for the char constructor.

--- implementation/test/model/MobileLeafTest.groovy
+++ implementation/test/model/MobileLeafTest.groovy
@@ -4,6 +4,14 @@ package model
  * @author Hendrik Werner // s4549775
  */
 class MobileLeafTest extends GroovyTestCase {
+    void testCharConstructor() {
+        new MobileLeaf("B" as char)
+        new MobileLeaf("b" as char)
+        new MobileLeaf("R" as char)
+        new MobileLeaf("r" as char)
+        shouldFail { new MobileLeaf("A" as char) }
+    }
+
     void testToString() {
         assert new MobileLeaf(MobileLeaf.Color.Black).toString() == "B"
         assert new MobileLeaf(MobileLeaf.Color.Red).toString() == "R"
