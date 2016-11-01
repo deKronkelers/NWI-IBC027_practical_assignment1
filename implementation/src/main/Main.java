d71496182bd2a1b5e1e14074d71e9440dbe40401
Hendrikto <hendrik.to@gmail.com>
2016-11-01 23:21:42 +0100

    Read in a mobile from stdin and print it to stdout.

--- implementation/src/main/Main.java
+++ implementation/src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import model.Mobile;
+
+import java.util.Scanner;
+
 /**
  * @author Hendrik Werner // s4549775
  * @author Constantin Blach // s4329872
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.print(
+                new Mobile(new Scanner(System.in).nextLine())
+        );
     }
 }
