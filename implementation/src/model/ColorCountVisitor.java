2da1f4c5ebc3832c46056318889911d64bebc63b
Hendrikto <hendrik.to@gmail.com>
2016-11-13 16:55:54 +0100

    Add a visitor that counts the leaf colors.

---
+++ implementation/src/model/ColorCountVisitor.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * @author hendrik
+ */
+public class ColorCountVisitor implements Visitor {
+    private int red;
+    private int black;
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getBlack() {
+        return black;
+    }
+
+    @Override
+    public void visit(MobileLeaf leaf) {
+        switch (leaf.getColor()) {
+            case Black:
+                black++;
+                break;
+            case Red:
+                red++;
+                break;
+        }
+    }
+
+    @Override
+    public void visit(Mobile mobile) {
+        mobile.getLeftChild().accept(this);
+        mobile.getRightChild().accept(this);
+    }
+}
